d262f8cc7b25827e31e21b9ba90020282c859eb4
podestal <luisrodriguezpodesta@gmail.com>
2024-06-22 09:14:35 -0500

    types

--- lesson1/build/js/main.js
+++ lesson1/build/js/main.js
@@ -1,8 +1,25 @@
 "use strict";
+// LESSON 1
 let username = 'John';
 console.log(username);
 let a = 12;
 let b = 6;
 let c = 2;
-console.log(a / b);
-console.log(c * b);
+// console.log(a / b);
+// console.log(c * b);
+// LESSON 2
+let myName = 'Dave';
+let meaningOfLife;
+let isLoading;
+let album;
+myName = 'John';
+meaningOfLife = 42;
+isLoading = true;
+album = 5150;
+const sum = (a, b) => {
+    return a + b;
+};
+// console.log(sum(14, 20))
+let postId;
+let isActive;
+let re = /\w+/g;
--- lesson1/src/main.ts
+++ lesson1/src/main.ts
@@ -1,10 +1,33 @@
+// LESSON 1
+
 let username = 'John'
 console.log(username)
 
 let a = 12 
 let b = 6
 let c = 2
-console.log(a / b);
-console.log(c * b);
+// console.log(a / b);
+// console.log(c * b);
+
+// LESSON 2
+let myName: string = 'Dave'
+let meaningOfLife: number
+let isLoading: boolean
+let album: any
+
+myName = 'John'
+meaningOfLife = 42
+isLoading = true
+album = 5150
+
+const sum = (a: number , b: string) => {
+    return a + b
+}
+
+// console.log(sum(14, 20))
+let postId: string | number
+let isActive: number | boolean | string
+let re: RegExp = /\w+/g
+
 
 
